987637588a831ef04d3eec6165cc9cb601511bd7
WaqiMTC <20B2068@ubd.edu.bn>
2023-09-18 19:47:47 +0800

    Update 03-backtracking.R
    lintr and styler may be a godsent
    will continue the rest later!

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -10,28 +10,33 @@ backtrack <- function(x) {
   current <- 2
   below_init <- FALSE
   above_init <- FALSE
-  
+
   while (current <= length(x)) {
     # check for when current value is less than initial
-    if (x[current] < initial & (below_init == FALSE)) {
+    if (x[current] < initial
+        && (below_init == FALSE)) {
       below_init <- TRUE
     }
-    
+
     # check for when current value is higher than initial
     # but only after below_init is TRUE
-    if ((x[current] > initial) & (below_init == TRUE) & (above_init == FALSE)) {
+    if ((x[current] > initial)
+        && (below_init == TRUE)
+        && (above_init == FALSE)) {
       above_init <- TRUE
     }
-    
+
     # increment current by 1
     current <- current + 1
   }
-  
+
   ifelse(below_init & above_init, TRUE, FALSE)
 }
 
 ## doing the actual filtering with the function
-backtracks_df <- 
+backtracks_df <-
   collatz_df %>%
-  filter(length >= 3,
-         sapply(seq, backtrack))
+  filter(
+    length >= 3,
+    sapply(seq, backtrack)
+  )
