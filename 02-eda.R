76be1c0a263146e2dd3a215e54ee8b15a3be03a7
Ros-2646 <23b2086@ubd.edu.bn>
2023-09-21 17:25:16 +0800

    Update 02-eda.R
    Added top10 and top10longest,
    For part 1

--- 02-eda.R
+++ 02-eda.R
@@ -1,1 +1,12 @@
 # Task 2
+
+library(tidyverse)
+
+## 1. Find the top 10 integers that produce the longest sequence
+   #Finding the top 10 integers with longest sequence
+top10 <- collatz_df %>%
+  arrange(desc(length)) %>%
+  head(10)
+   #Extracting the integers with longest sequence
+top10longest <- top10 %>%
+  pull("start")
