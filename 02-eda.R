35b66ab0d6904bc17e9d637cfa0ca69ca4cf97e9
Ros-2646 <23b2086@ubd.edu.bn>
2023-09-21 17:40:26 +0800

    Update 02-eda.R
    For part 1,
    removed top10.
    For part 2,
    added max_val_int

--- 02-eda.R
+++ 02-eda.R
@@ -2,11 +2,16 @@
 
 library(tidyverse)
 
-## 1. Find the top 10 integers that produce the longest sequence
-   #Finding the top 10 integers with longest sequence
-top10 <- collatz_df %>%
+## 1. Find the top 10 integers that produce the longest sequence.
+
+top10longest <- collatz_df %>%
   arrange(desc(length)) %>%
-  head(10)
-   #Extracting the integers with longest sequence
-top10longest <- top10 %>%
+  head(10)%>%
+  pull("start")
+
+## 2. Find out which starting integer produces a sequence that reaches the highest max value.
+
+max_val_int <- collatz_df %>%
+  arrange(desc(max_val))%>%
+  head(1)%>%
   pull("start")
