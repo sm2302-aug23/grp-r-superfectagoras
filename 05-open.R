9b27cfbc28ed7cf84104eb8bada99f1dc69b2c57
fizahjamal <19b9011@ubd.edu.bn>
2023-09-23 15:12:48 +0800

    Update 05-open.R

--- 05-open.R
+++ 05-open.R
@@ -1,1 +1,5 @@
 # Task 5
+
+# Question: Are there common stopping times that appear frequently across different starting integeres?
+
+#Below is the Rcode examples to investigate and understand the answer to the question above
